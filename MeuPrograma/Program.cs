4dffcdac148c76223391472b48ee7b95b4ea2054
DiogoMeiraFreire <didi9934@gmail.com>
2024-02-21 19:59:04 +0000

    Add summary to Program and Main

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// The MeuPrograma's main class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Sends two messages: "Hello, World!" and "Bye world"
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
