52804fd95a44f7ce0b1de2ea141adf39b8f820b6
DiogoMeiraFreire <didi9934@gmail.com>
2024-02-23 15:11:20 +0000

    Added input with changed outputs

---
+++ AskForInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write an integer");
+            string input = Console.ReadLine();
+
+            int i = int.Parse(input);
+            Console.WriteLine($"this is in i:-> {i}");
+            
+            Console.WriteLine("Write a floating-point number");
+            string input2 = Console.ReadLine();
+
+            float f = float.Parse(input2);
+
+            var result = i + f;
+            Console.WriteLine($"Here's the result->{result}");
+
+        }
+    }
+}
