7700e5eaa12a00afd7069b78feadafb242f6f95d
DiogoMeiraFreire <didi9934@gmail.com>
2024-02-23 14:47:05 +0000

    Added more strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -9,10 +9,15 @@ namespace ManyStrings
             // string variables
             string unicode = "\u03A9";
             string escape = "this is a tab \t a tab has occurred";
+            string conc = "ab " + 2;
+            string inter = $"The unicode variable is {unicode}";
+
 
             // prints
             Console.WriteLine(unicode);
             Console.WriteLine(escape);
+            Console.WriteLine(conc);
+            Console.WriteLine(inter);
         }
     }
 }
