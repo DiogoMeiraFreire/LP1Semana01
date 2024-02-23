5b2ecf28f0f58b15c495e9e3e2dae3bee82840b3
DiogoMeiraFreire <didi9934@gmail.com>
2024-02-23 14:59:17 +0000

    Added formatting to the variables asked

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,12 +12,20 @@ namespace ManyStrings
             string conc = "ab " + 2;
             string inter = $"The unicode variable is {unicode}";
 
+            double xx = 1.23456;
+            int ii = 19;
+
 
             // prints
             Console.WriteLine(unicode);
             Console.WriteLine(escape);
             Console.WriteLine(conc);
             Console.WriteLine(inter);
+
+            Console.WriteLine(xx.ToString("F2"));
+            Console.WriteLine($"{xx:p1}");
+            Console.WriteLine(ii.ToString("X"));
+            Console.WriteLine(ii.ToString("C"));
         }
     }
 }
